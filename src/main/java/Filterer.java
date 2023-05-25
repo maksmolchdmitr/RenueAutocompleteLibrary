3e83b3d138e18d8111b5160f02814c7fb7d3b340
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-05-25 19:53:26 +0300

    fix Filterer

--- src/main/java/Filterer.java
+++ src/main/java/Filterer.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Filterer {
-    private static final String ARGUMENT_TEMPLATE = "column\\[(\\d+)]([<>=]+)(?!\\()(\\S+)(?<!\\))";
+    private static final String ARGUMENT_TEMPLATE = "column\\[(\\d+)]\\s*([<>=]+)\\s*(\"[^\"]+\"|\\S+)";
     private final String filter;
 
     public Filterer(String filter) {
@@ -15,12 +15,26 @@ public final class Filterer {
     }
 
     public boolean matchesFilter(String text) {
+        if (filter.isEmpty()) return true;
         String[] values = text.split(",");
         String boolFilter = getBoolFilter(values);
+        return getResult(boolFilter);
+    }
+
+    private static boolean getResult(String boolFilter) {
         JexlEngine jexlEngine = new JexlBuilder().create();
         JexlExpression jexlExpression = jexlEngine.createExpression(boolFilter);
         JexlContext context = new MapContext();
-        return (long) jexlExpression.evaluate(context) == 1;
+        boolean result;
+        var res = jexlExpression.evaluate(context);
+        if (res instanceof Long) {
+            result = (long) res == 1;
+        } else if (res instanceof Boolean) {
+            result = (boolean) res;
+        } else {
+            result = (int) res == 1;
+        }
+        return result;
     }
 
     private String getBoolFilter(String[] values) {
@@ -39,15 +53,21 @@ public final class Filterer {
     }
 
     public boolean checkArgument(Argument argument, String[] values) {
+        Comparable arg = values[argument.columnNum - 1], value = argument.value;
+        try {
+            arg = Integer.parseInt((String) arg);
+            value = Integer.parseInt((String) value);
+        } catch (NumberFormatException ignored) {
+        }
         switch (argument.sign) {
             case EQUAL:
-                return values[argument.columnNum - 1].equals(argument.value);
+                return arg.equals(value);
             case LESS:
-                return values[argument.columnNum - 1].compareTo(argument.value) < 0;
+                return arg.compareTo(value) < 0;
             case GREATER:
-                return values[argument.columnNum - 1].compareTo(argument.value) > 0;
+                return arg.compareTo(value) > 0;
             case NOT_EQUAL:
-                return !values[argument.columnNum - 1].equals(argument.value);
+                return !arg.equals(value);
             default:
                 throw new RuntimeException();
         }
